7797d5c8f91a0433d73b4a44e5ff10d11c92565f
Greg Russell <gfr10598@gmail.com>
2018-05-08 13:01:33 -0400

    use uint64

--- inetdiag/inetdiag.go
+++ inetdiag/inetdiag.go
@@ -89,7 +89,7 @@ type InetDiagSockID struct {
 	IDiagSrc    [16]byte
 	IDiagDst    [16]byte
 	IDiagIf     uint32
-	IDiagCookie [2]uint32
+	IDiagCookie uint64
 }
 
 // SrcIP returns a golang net encoding of source address.
