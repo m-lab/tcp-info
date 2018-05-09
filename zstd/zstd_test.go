9aa8e6ba3dbdbeedef438dd31e55508292090279
Greg Russell <gfr10598@gmail.com>
2018-05-09 13:12:34 -0400

    Use TempDir and cleanup

--- zstd/zstd_test.go
+++ zstd/zstd_test.go
@@ -2,18 +2,32 @@ package zstd_test
 
 import (
 	"io"
+	"io/ioutil"
+	"os/exec"
 	"testing"
 
 	"github.com/m-lab/tcp-info/zstd"
 )
 
 func TestReader(t *testing.T) {
+	tmpdir, err := ioutil.TempDir(".", "tmp")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		cmd := exec.Command("rm", "-rf", tmpdir)
+		err = cmd.Run()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
 	data := make([]byte, 10000)
 	for i := range data {
 		data[i] = byte((i * 37) % 256)
 	}
 
-	w, wg := zstd.NewWriter("./test.zst")
+	w, wg := zstd.NewWriter(tmpdir + "/test.zst")
 	n, err := w.Write(data)
 	if err != nil {
 		t.Fatal(err)
@@ -22,7 +36,7 @@ func TestReader(t *testing.T) {
 	wg.Wait()
 
 	read := make([]byte, 20000)
-	r := zstd.NewReader("./test.zst")
+	r := zstd.NewReader(tmpdir + "/test.zst")
 	// Interesting...  Sometimes this requires multiple calls to read.
 	n, err = io.ReadAtLeast(r, read, 10000)
 	if err != nil {
