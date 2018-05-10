65a778a786fa61dfa663d45c3ab4d128a533aed4
Greg Russell <gfr10598@gmail.com>
2018-05-10 11:45:58 -0400

    redundant enums, improve comments and remove LOG

--- inetdiag/inetdiag.go
+++ inetdiag/inetdiag.go
@@ -78,30 +78,6 @@ const (
 	INET_DIAG_MAX
 )
 
-// inet_diag.h
-const (
-	INET_DIAG_NONE = iota
-	INET_DIAG_MEMINFO
-	INET_DIAG_INFO
-	INET_DIAG_VEGASINFO
-	INET_DIAG_CONG
-	INET_DIAG_TOS
-	INET_DIAG_TCLASS
-	INET_DIAG_SKMEMINFO
-	INET_DIAG_SHUTDOWN
-	INET_DIAG_DCTCPINFO
-	INET_DIAG_PROTOCOL
-	INET_DIAG_SKV6ONLY
-	INET_DIAG_LOCALS
-	INET_DIAG_PEERS
-	INET_DIAG_PAD
-	INET_DIAG_MARK
-	INET_DIAG_BBRINFO
-	INET_DIAG_CLASS_ID
-	INET_DIAG_MD5SIG
-	INET_DIAG_MAX
-)
-
 var diagFamilyMap = map[uint8]string{
 	syscall.AF_INET:  "tcp",
 	syscall.AF_INET6: "tcp6",
@@ -133,7 +109,6 @@ func ip(bytes [16]byte) net.IP {
 	if isIpv6(bytes) {
 		return ipv6(bytes)
 	} else {
-		log.Println("ipv4", ipv4(bytes))
 		return ipv4(bytes)
 	}
 }
