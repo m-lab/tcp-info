398401e242232a833f62f668d10f2a480876f729
Greg Russell <gfr10598@gmail.com>
2018-05-14 14:50:19 -0400

    use sys/unix instead of vishvananda

--- inetdiag/inetdiag.go
+++ inetdiag/inetdiag.go
@@ -37,7 +37,7 @@ import (
 	"unsafe"
 
 	tcpinfo "github.com/m-lab/tcp-info/nl-proto"
-	"github.com/vishvananda/netlink/nl"
+	"golang.org/x/sys/unix"
 )
 
 // Error types.
@@ -186,12 +186,6 @@ func (msg *InetDiagMsg) String() string {
 	return fmt.Sprintf("%s, %s, %s", diagFamilyMap[msg.IDiagFamily], tcpinfo.TCPState(msg.IDiagState), msg.ID.String())
 }
 
-// rtaAlignOf round the length of a netlink route attribute up to align it
-// properly.
-func rtaAlignOf(attrlen int) int {
-	return (attrlen + syscall.RTA_ALIGNTO - 1) & ^(syscall.RTA_ALIGNTO - 1)
-}
-
 // ParseInetDiagMsg returns the InetDiagMsg itself, and the aligned byte array containing the message content.
 // Modified from original to also return attribute data array.
 func ParseInetDiagMsg(data []byte) (*InetDiagMsg, []byte) {
@@ -232,7 +226,7 @@ func Parse(msg *syscall.NetlinkMessage, skipLocal bool) (*ParsedMessage, error) 
 		}
 	}
 	parsedMsg := ParsedMessage{Header: msg.Header, InetDiagMsg: idm}
-	attrs, err := nl.ParseRouteAttr(attrBytes)
+	attrs, err := ParseRouteAttr(attrBytes)
 	if err != nil {
 		return nil, err
 	}
@@ -240,4 +234,37 @@ func Parse(msg *syscall.NetlinkMessage, skipLocal bool) (*ParsedMessage, error) 
 		parsedMsg.Attributes[attrs[i].Attr.Type] = &attrs[i]
 	}
 	return &parsedMsg, nil
+}
+
+/*********************************************************************************************/
+/*             Copied from "github.com/vishvananda/netlink/nl/nl_linux.go"                   */
+/*********************************************************************************************/
+
+// ParseRouteAttr parses a byte array into a NetlinkRouteAttr struct.
+func ParseRouteAttr(b []byte) ([]syscall.NetlinkRouteAttr, error) {
+	var attrs []syscall.NetlinkRouteAttr
+	for len(b) >= unix.SizeofRtAttr {
+		a, vbuf, alen, err := netlinkRouteAttrAndValue(b)
+		if err != nil {
+			return nil, err
+		}
+		// TODO - Resolve this ugly cross-over between sys/unix and syscall.
+		ra := syscall.NetlinkRouteAttr{Attr: syscall.RtAttr(*a), Value: vbuf[:int(a.Len)-unix.SizeofRtAttr]}
+		attrs = append(attrs, ra)
+		b = b[alen:]
+	}
+	return attrs, nil
+}
+
+// rtaAlignOf rounds the length of a netlink route attribute up to align it properly.
+func rtaAlignOf(attrlen int) int {
+	return (attrlen + unix.RTA_ALIGNTO - 1) & ^(unix.RTA_ALIGNTO - 1)
+}
+
+func netlinkRouteAttrAndValue(b []byte) (*unix.RtAttr, []byte, int, error) {
+	a := (*unix.RtAttr)(unsafe.Pointer(&b[0]))
+	if int(a.Len) < unix.SizeofRtAttr || int(a.Len) > len(b) {
+		return nil, nil, 0, unix.EINVAL
+	}
+	return a, b[unix.SizeofRtAttr:], rtaAlignOf(int(a.Len)), nil
 }
