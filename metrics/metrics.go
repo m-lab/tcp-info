237b8e4384ffc372011faeffde4fb0c10b36cfe2
Greg Russell <gfr10598@gmail.com>
2018-08-01 16:22:01 -0400

    remove 9090 support

--- metrics/metrics.go
+++ metrics/metrics.go
@@ -10,7 +10,6 @@ package metrics
 import (
 	"math"
 	"net/http"
-	"net/http/pprof"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -23,20 +22,6 @@ func init() {
 func SetupPrometheus() {
 	http.Handle("/metrics", promhttp.Handler())
 
-	// Define a custom serve mux for prometheus to listen on a separate port.
-	// We listen on a separate port so we can forward this port on the host VM.
-	// We cannot forward port 8080 because it is used by AppEngine.
-	mux := http.NewServeMux()
-	// Assign the default prometheus handler to the standard exporter path.
-	mux.Handle("/metrics", promhttp.Handler())
-	// Assign the pprof handling paths to the external port to access individual
-	// instances.
-	mux.HandleFunc("/debug/pprof/", pprof.Index)
-	mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
-
 	prometheus.MustRegister(FetchTimeSummary)
 
 	prometheus.MustRegister(EntryFieldCountHistogram)
@@ -47,7 +32,6 @@ func SetupPrometheus() {
 	prometheus.MustRegister(FileCount)
 	prometheus.MustRegister(ErrorCount)
 	prometheus.MustRegister(WarningCount)
-	go http.ListenAndServe(":9090", mux)
 }
 
 var (
