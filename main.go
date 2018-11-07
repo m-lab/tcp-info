644ffb66b45669c424f522816e47d98246f34259
Greg Russell <gfr10598@gmail.com>
2018-11-07 16:46:44 -0500

    remove obsolete code

--- main.go
+++ main.go
@@ -6,12 +6,10 @@ package main
 import (
 	"encoding/binary"
 	"flag"
-	"fmt"
 	"io"
 	"log"
 	"os"
 	"runtime/trace"
-	"sync"
 	"syscall"
 	"time"
 
@@ -19,13 +17,10 @@ import (
 
 	"github.com/golang/protobuf/proto"
 
-	"github.com/m-lab/tcp-info/cache"
 	"github.com/m-lab/tcp-info/inetdiag"
 	"github.com/m-lab/tcp-info/metrics"
 	tcp "github.com/m-lab/tcp-info/nl-proto"
-	"github.com/m-lab/tcp-info/nl-proto/pbtools"
 	"github.com/m-lab/tcp-info/saver"
-	"github.com/m-lab/tcp-info/zstd"
 )
 
 /*
@@ -63,44 +58,6 @@ func init() {
 //  2. the go zstd wrapper doesn't seem to work well - poor compression and slow.
 //  3. zstd seems to result in similar file size using proto or raw output.
 
-func Marshal(filename string, marshaler chan *inetdiag.ParsedMessage, wg *sync.WaitGroup) error {
-	out, err := zstd.NewWriter(filename)
-	if err != nil {
-		return err
-	}
-	count := 0
-	for {
-		count++
-		msg, ok := <-marshaler
-		if !ok {
-			break
-		}
-		p := pbtools.CreateProto(msg.Timestamp, msg.Header, msg.InetDiagMsg, msg.Attributes[:])
-		m, err := proto.Marshal(p)
-		if err != nil {
-			log.Println(err)
-		} else {
-			out.Write(m)
-		}
-	}
-	out.Close()
-	wg.Done()
-	return nil
-}
-
-var marshallerChannels []chan *inetdiag.ParsedMessage
-
-func Queue(msg *inetdiag.ParsedMessage) {
-	q := marshallerChannels[int(msg.InetDiagMsg.IDiagInode)%len(marshallerChannels)]
-	q <- msg
-}
-
-func CloseAll() {
-	for i := range marshallerChannels {
-		close(marshallerChannels[i])
-	}
-}
-
 var totalCount = 0
 var errCount = 0
 var localCount = 0
@@ -116,43 +73,27 @@ func Stats() {
 		diffCount, newCount, errCount)
 }
 
-func ParseAndQueue(cache *cache.Cache, msg *syscall.NetlinkMessage, queue bool) *inetdiag.ParsedMessage {
+func Parse(msg *syscall.NetlinkMessage) *inetdiag.ParsedMessage {
 	totalCount++
 	pm, err := inetdiag.Parse(msg, true)
 	if err != nil {
 		log.Println(err)
 		errCount++
-	} else if pm == nil {
-		localCount++
-	} else {
-		if !queue {
-			return pm
-		}
-		old := cache.Update(pm)
-		if old == nil {
-			newCount++
-			Queue(pm)
-		} else {
-			if pbtools.Compare(pm, old) > 0 {
-				if rawOut != nil {
-					binary.Write(rawOut, binary.BigEndian, msg.Header)
-					binary.Write(rawOut, binary.BigEndian, msg.Data)
-				}
-				diffCount++
-				Queue(pm)
-			}
-		}
+		return nil
 	}
-	return nil
+	if pm == nil {
+		localCount++
+	}
+	return pm
 }
 
-func Demo(cache *cache.Cache, svr chan<- []*inetdiag.ParsedMessage) (int, int) {
+func Demo(svr chan<- []*inetdiag.ParsedMessage) (int, int) {
 	all := make([]*inetdiag.ParsedMessage, 0, 500)
 	remoteCount := 0
 	res6, _ := inetdiag.OneType(syscall.AF_INET6) // Ignoring errors in Demo code
 	ts := time.Now()
 	for i := range res6 {
-		pm := ParseAndQueue(cache, res6[i], false)
+		pm := Parse(res6[i])
 		if pm != nil {
 			pm.Timestamp = ts
 			all = append(all, pm)
@@ -162,7 +103,7 @@ func Demo(cache *cache.Cache, svr chan<- []*inetdiag.ParsedMessage) (int, int) {
 	res4, _ := inetdiag.OneType(syscall.AF_INET) // Ignoring errors in Demo code
 	ts = time.Now()
 	for i := range res4 {
-		pm := ParseAndQueue(cache, res4[i], false)
+		pm := Parse(res4[i])
 		if pm != nil {
 			pm.Timestamp = ts
 			all = append(all, pm)
@@ -192,34 +133,15 @@ func NextMsg(rdr io.Reader) (*syscall.NetlinkMessage, error) {
 }
 
 var (
-	filter      = flag.Bool("filter", true, "Record only records that change")
 	reps        = flag.Int("reps", 0, "How many cycles should be recorded, 0 means continuous")
-	verbose     = flag.Bool("v", false, "Enable verbose logging")
-	numFiles    = flag.Int("files", 1, "Number of output files")
 	enableTrace = flag.Bool("trace", false, "Enable trace")
-	rawFile     = flag.String("raw", "", "File to write raw records to")
-	source      = flag.String("source", "", "Source to read (uncompressed) NetlinkMessage records")
 	promPort    = flag.Int("prom", 9090, "Prometheus metrics export port")
 )
-
-var rawOut io.WriteCloser
 
 func main() {
 	// TODO - use flagx.ArgsFromEnv
 
 	metrics.SetupPrometheus(*promPort)
-
-	if *rawFile != "" {
-		log.Println("Raw output to", *rawFile)
-		var err error
-		rawOut, err = zstd.NewWriter(*rawFile)
-		if err != nil {
-			log.Fatal("Could not open raw output file", *rawFile)
-		}
-		defer rawOut.Close()
-	}
-
-	msgCache := cache.NewCache()
 
 	p := tcp.TCPDiagnosticsProto{}
 	p.TcpInfo = &tcp.TCPInfoProto{}
@@ -244,56 +166,30 @@ func main() {
 		defer trace.Stop()
 	}
 
-	var wg sync.WaitGroup
-	for i := 0; i < *numFiles; i++ {
-		marshChan := make(chan *inetdiag.ParsedMessage, 1000)
-		marshallerChannels = append(marshallerChannels, marshChan)
-		fn := fmt.Sprintf("file%02d.zst", i)
-		wg.Add(1)
-		go Marshal(fn, marshChan, &wg)
-	}
+	totalCount := 0
+	remoteCount := 0
+	loops := 0
+	svr := saver.NewSaver("host", "pod", 3)
 
-	if *source != "" {
-		log.Println("Reading messages from", *source)
-		rdr := zstd.NewReader(*source)
-		for {
-			msg, err := NextMsg(rdr)
-			if err != nil {
-				break
-			}
-			ParseAndQueue(msgCache, msg, true)
-		}
-		Stats()
-	} else {
-		totalCount := 0
-		remoteCount := 0
-		loops := 0
-		svr := saver.NewSaver("host", "pod", 3)
+	// Construct message channel, buffering up to 2 batches of messages without stalling producer.
+	// We may want to increase this if we observe main() stalling.
+	svrChan := make(chan []*inetdiag.ParsedMessage, 2)
+	go svr.MessageSaverLoop(svrChan)
 
-		// Construct message channel, buffering up to 2 batches of messages without stalling producer.
-		// We may want to increase this if we observe main() stalling.
-		svrChan := make(chan []*inetdiag.ParsedMessage, 2)
-		go svr.MessageSaverLoop(svrChan)
-
-		for loops = 0; *reps == 0 || loops < *reps; loops++ {
-			total, remote := Demo(msgCache, svrChan)
-			totalCount += total
-			remoteCount += remote
-			if loops%10000 == 0 {
-				Stats()
-				svr.Stats()
-			}
-		}
-		close(svrChan)
-		svr.Done.Wait()
-		Stats()
-		if loops > 0 {
-			log.Println(totalCount, "sockets", remoteCount, "remotes", totalCount/loops, "per iteration")
+	for loops = 0; *reps == 0 || loops < *reps; loops++ {
+		total, remote := Demo(svrChan)
+		totalCount += total
+		remoteCount += remote
+		if loops%10000 == 0 {
+			Stats()
+			svr.Stats()
 		}
 	}
 
-	//log.Printf("%+v\n", delta.DiffCounts)
-	CloseAll()
-	wg.Wait()
-
+	close(svrChan)
+	svr.Done.Wait()
+	Stats()
+	if loops > 0 {
+		log.Println(totalCount, "sockets", remoteCount, "remotes", totalCount/loops, "per iteration")
+	}
 }
