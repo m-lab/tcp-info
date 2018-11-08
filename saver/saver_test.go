d98f4719bf2af363541222f541109076c4a37688
Greg Russell <gfr10598@gmail.com>
2018-11-08 14:09:07 -0500

    Good TestBasic for Saver

--- saver/saver_test.go
+++ saver/saver_test.go
@@ -23,57 +23,46 @@ func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
-func TestBasic(t *testing.T) {
+func dump(mp *inetdiag.ParsedMessage) {
+	for i := range mp.Attributes {
+		a := mp.Attributes[i]
+		if a != nil {
+			log.Printf("%d %d %+v\n", i, len(a.Value), a)
+		}
+	}
+}
+
+func msg(cookie uint64, dport uint16) *inetdiag.ParsedMessage {
 	var json1 = `{"Header":{"Len":356,"Type":20,"Flags":2,"Seq":1,"Pid":148940},"Data":"CgEAAOpWE6cmIAAAEAMEFbM+nWqBv4ehJgf4sEANDAoAAAAAAAAAgQAAAAAdWwAAAAAAAAAAAAAAAAAAAAAAAAAAAAC13zIBBQAIAAAAAAAFAAUAIAAAAAUABgAgAAAAFAABAAAAAAAAAAAAAAAAAAAAAAAoAAcAAAAAAICiBQAAAAAAALQAAAAAAAAAAAAAAAAAAAAAAAAAAAAArAACAAEAAAAAB3gBQIoDAECcAABEBQAAuAQAAAAAAAAAAAAAAAAAAAAAAAAAAAAAUCEAAAAAAAAgIQAAQCEAANwFAACsywIAJW8AAIRKAAD///9/CgAAAJQFAAADAAAALMkAAIBwAAAAAAAALnUOAAAAAAD///////////ayBAAAAAAASfQPAAAAAADMEQAANRMAAAAAAABiNQAAxAsAAGMIAABX5AUAAAAAAAoABABjdWJpYwAAAA=="}`
 	nm := syscall.NetlinkMessage{}
 	err := json.Unmarshal([]byte(json1), &nm)
 	if err != nil {
-		t.Fatal(err)
+		log.Println(err)
+		return nil
 	}
 	mp, err := inetdiag.Parse(&nm, true)
 	if err != nil {
-		t.Fatal(err)
+		log.Println(err)
+		return nil
+	}
+	for i := 0; i < 8; i++ {
+		mp.InetDiagMsg.ID.IDiagCookie[i] = byte(cookie & 0x0FF)
+		cookie <<= 8
+	}
+	for i := 0; i < 2; i++ {
+		mp.InetDiagMsg.ID.IDiagDPort[i] = byte(dport & 0x0FF)
+		dport <<= 8
 	}
 	log.Printf("%+v\n", mp)
+	return mp
+}
 
-	svr := saver.NewSaver("foo", "bar", 1)
-	svrChan := make(chan []*inetdiag.ParsedMessage, 0) // no buffering
-	go svr.MessageSaverLoop(svrChan)
-
-	messages := []*inetdiag.ParsedMessage{mp}
-	// This round just initialized the cache.
-	svrChan <- messages
-
-	if len(svr.Connections) != 1 {
-		t.Fatal("Connections")
-	}
-
-	filename := "00010101Z000000.000U00000000L2620:0:1003:415:b33e:9d6a:81bf:87a1:59990R2607:f8b0:400d:c0a::81:5031_00000.zst"
+func cleanup(t *testing.T, size int64, filename string) {
 	info, err := os.Stat(filename)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// This should NOT write to file, because nothing changed
-	//	svrChan <- messages
-
-	mp.Attributes[2].Value[20] += 1
-	// This should write to file (through go routine)
-	svrChan <- messages
-
-	mp.Attributes[2].Value[105] += 1
-	// This should write to file (through go routine)
-	svrChan <- messages
-
-	// Force close all the files.
-	close(svrChan)
-	svr.Done.Wait()
-
-	info, err = os.Stat(filename)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if info.Size() != 250 {
+	if info.Size() != size {
 		t.Error("Size:", info.Size())
 	}
 	log.Printf("%+v\n", info)
@@ -82,4 +71,38 @@ func TestBasic(t *testing.T) {
 	if err == nil {
 		t.Fatal("Failed to remove file")
 	}
+}
+func TestBasic(t *testing.T) {
+	svr := saver.NewSaver("foo", "bar", 1)
+	svrChan := make(chan []*inetdiag.ParsedMessage, 0) // no buffering
+	go svr.MessageSaverLoop(svrChan)
+
+	// This round just initializes the cache.
+	m1 := []*inetdiag.ParsedMessage{msg(1234, 1234), msg(234, 234)}
+	dump(m1[0])
+	svrChan <- m1
+
+	// This should NOT write to file, because nothing changed
+	m2 := []*inetdiag.ParsedMessage{msg(1234, 1234), msg(234, 234)}
+	svrChan <- m2
+
+	// This changes the first connection, and ends the secon connection.
+	m3 := []*inetdiag.ParsedMessage{msg(1234, 1234)}
+	m3[0].Attributes[inetdiag.INET_DIAG_INFO].Value[20] = 127
+	svrChan <- m3
+
+	// This changes the first connection again.
+	m4 := []*inetdiag.ParsedMessage{msg(1234, 1234)}
+	m3[0].Attributes[inetdiag.INET_DIAG_INFO].Value[20] = 127
+	m4[0].Attributes[inetdiag.INET_DIAG_INFO].Value[105] = 127
+	svrChan <- m4
+
+	m5 := []*inetdiag.ParsedMessage{msg(1234, 1234)}
+	svrChan <- m5
+
+	// Force close all the files.
+	close(svrChan)
+	svr.Done.Wait()
+	cleanup(t, 271, "00010101Z000000.000U00000000L2620:0:1003:415:b33e:9d6a:81bf:87a1:59990R2607:f8b0:400d:c0a::81:53760_00000.zst")
+	cleanup(t, 248, "00010101Z000000.000U00000000L2620:0:1003:415:b33e:9d6a:81bf:87a1:59990R2607:f8b0:400d:c0a::81:59904_00000.zst")
 }
