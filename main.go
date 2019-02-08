b1d84510e819d88f6fef85444f97a2629ad5ba58
Gregory Russell <gfr@google.com>
2019-02-07 19:03:55 -0500

    rate limit at 100Hz

--- main.go
+++ main.go
@@ -7,11 +7,12 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime"
 	"runtime/trace"
 	"syscall"
 	"time"
 
-	_ "net/http/pprof"
+	_ "net/http/pprof" // Support profiling
 
 	"github.com/golang/protobuf/proto"
 
@@ -124,6 +125,10 @@ var (
 func main() {
 	// TODO - use flagx.ArgsFromEnv
 
+	// Performance instrumentation.
+	runtime.SetBlockProfileRate(1000000) // 1 sample/msec
+	runtime.SetMutexProfileFraction(1000)
+
 	metrics.SetupPrometheus(*promPort)
 
 	p := tcp.TCPDiagnosticsProto{}
@@ -159,12 +164,26 @@ func main() {
 	svrChan := make(chan []*inetdiag.ParsedMessage, 2)
 	go svr.MessageSaverLoop(svrChan)
 
+	nextTime := time.Now()
+
 	for loops = 0; *reps == 0 || loops < *reps; loops++ {
 		total, remote := CollectDefaultNamespace(svrChan)
 		totalCount += total
 		remoteCount += remote
-		if loops%10000 == 0 {
+		// print stats roughly once per minute.
+		if loops%6000 == 0 {
 			Stats(svr)
+		}
+
+		// For now, cap rate at 100 Hz.
+		nextTime = nextTime.Add(10 * time.Millisecond)
+		// If we get more than 5 msec behind, clamp there, to avoid bursts.
+		if nextTime.Before(time.Now().Add(5 * time.Millisecond)) {
+			nextTime = time.Now().Add(5 * time.Millisecond)
+		}
+		delay := nextTime.Sub(time.Now())
+		if delay > 0 {
+			time.Sleep(delay)
 		}
 	}
 
