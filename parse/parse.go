68c67c34ceab19db94a1d61b779a286059ec7c6c
Greg Russell <gfr10598@gmail.com>
2019-03-28 22:37:25 -0400

    more fields and better logging

--- parse/parse.go
+++ parse/parse.go
@@ -265,31 +265,44 @@ func (pm *ParsedMessage) Decode() (*Wrapper, error) {
 		if rta == nil {
 			continue
 		}
+		result.FieldMask |= 1 << uint8(i-1)
 		switch i {
 		case inetdiag.INET_DIAG_MEMINFO:
 			result.MemInfo = rta.ToMemInfo()
 		case inetdiag.INET_DIAG_INFO:
 			result.TcpInfo = rta.ToLinuxTCPInfo()
 		case inetdiag.INET_DIAG_VEGASINFO:
-
+			result.VegasInfo = rta.ToVegasInfo()
 		case inetdiag.INET_DIAG_CONG:
 			result.CongestionAlgorithm = rta.CongestionAlgorithm()
 		case inetdiag.INET_DIAG_TOS:
+			result.TOS = rta.ToTOS()
 		case inetdiag.INET_DIAG_TCLASS:
+			result.TClass = rta.ToTCLASS()
 		case inetdiag.INET_DIAG_SKMEMINFO:
 			result.SocketMem = rta.ToSockMemInfo()
 		case inetdiag.INET_DIAG_SHUTDOWN:
+			result.Shutdown = rta.ToShutdown()
 		case inetdiag.INET_DIAG_DCTCPINFO:
+			result.DCTCPInfo = rta.ToDCTCPInfo()
 		case inetdiag.INET_DIAG_PROTOCOL:
+			result.Protocol = rta.ToProtocol()
 		case inetdiag.INET_DIAG_SKV6ONLY:
+			log.Println("SKV6ONLY not handled", len(rta))
 		case inetdiag.INET_DIAG_LOCALS:
+			log.Println("LOCAL not handled", len(rta))
 		case inetdiag.INET_DIAG_PEERS:
+			log.Println("PEERS not handled", len(rta))
 		case inetdiag.INET_DIAG_PAD:
+			log.Println("PAD not handled", len(rta))
 		case inetdiag.INET_DIAG_MARK:
+			result.Mark = rta.ToMark()
 		case inetdiag.INET_DIAG_BBRINFO:
-			result.BBR = rta.ToBBRInfo()
+			result.BBRInfo = rta.ToBBRInfo()
 		case inetdiag.INET_DIAG_CLASS_ID:
+			log.Println("CLASS_ID not handled", len(rta))
 		case inetdiag.INET_DIAG_MD5SIG:
+			log.Println("MD5SIGnot handled", len(rta))
 		default:
 			// TODO metric so we can alert.
 			log.Println("unhandled attribute type:", i)
@@ -374,11 +387,46 @@ func maybeCopy(src []byte, size int) unsafe.Pointer {
 	return unsafe.Pointer(&src[0])
 }
 
+// ToMemInfo maps the raw RouteAttrValue onto a MemInfo.
+func (raw RouteAttrValue) ToMemInfo() *inetdiag.MemInfo {
+	structSize := (int)(unsafe.Sizeof(inetdiag.MemInfo{}))
+	return (*inetdiag.MemInfo)(maybeCopy(raw, structSize))
+}
+
 // ToLinuxTCPInfo maps the raw RouteAttrValue into a LinuxTCPInfo struct.
 // For older data, it may have to copy the bytes.
 func (raw RouteAttrValue) ToLinuxTCPInfo() *tcp.LinuxTCPInfo {
 	structSize := (int)(unsafe.Sizeof(tcp.LinuxTCPInfo{}))
 	return (*tcp.LinuxTCPInfo)(maybeCopy(raw, structSize))
+}
+
+// ToVegasInfo maps the raw RouteAttrValue onto a VegasInfo.
+// For older data, it may have to copy the bytes.
+func (raw RouteAttrValue) ToVegasInfo() *inetdiag.VegasInfo {
+	structSize := (int)(unsafe.Sizeof(inetdiag.VegasInfo{}))
+	return (*inetdiag.VegasInfo)(maybeCopy(raw, structSize))
+}
+
+// CongestionAlgorithm returns the congestion algorithm string
+// INET_DIAG_CONG
+func (raw RouteAttrValue) CongestionAlgorithm() string {
+	return string(raw)
+}
+
+func (raw RouteAttrValue) toUint8() uint8 {
+	if len(raw) < 1 {
+		log.Println("Parse error")
+		return 0
+	}
+	return uint8(raw[0])
+}
+
+func (raw RouteAttrValue) ToTOS() uint8 {
+	return raw.toUint8()
+}
+
+func (raw RouteAttrValue) ToTCLASS() uint8 {
+	return raw.toUint8()
 }
 
 // ToSockMemInfo maps the raw RouteAttrValue onto a SockMemInfo.
@@ -388,31 +436,33 @@ func (raw RouteAttrValue) ToSockMemInfo() *inetdiag.SocketMemInfo {
 	return (*inetdiag.SocketMemInfo)(maybeCopy(raw, structSize))
 }
 
-// ToMemInfo maps the raw RouteAttrValue onto a MemInfo.
-func (raw RouteAttrValue) ToMemInfo() *inetdiag.MemInfo {
-	structSize := (int)(unsafe.Sizeof(inetdiag.MemInfo{}))
-	return (*inetdiag.MemInfo)(maybeCopy(raw, structSize))
+func (raw RouteAttrValue) ToShutdown() uint8 {
+	return raw.toUint8()
 }
 
-// INET_DIAG_MARK
-//	if (tb[INET_DIAG_MARK])
-//		s->mark = rta_getattr_u32(tb[INET_DIAG_MARK]);
+// ToVegasInfo maps the raw RouteAttrValue onto a VegasInfo.
+// For older data, it may have to copy the bytes.
+func (raw RouteAttrValue) ToDCTCPInfo() *inetdiag.DCTCPInfo {
+	structSize := (int)(unsafe.Sizeof(inetdiag.DCTCPInfo{}))
+	return (*inetdiag.DCTCPInfo)(maybeCopy(raw, structSize))
+}
 
-// INET_DIAG_PROTOCOL
-// if (tb[INET_DIAG_PROTOCOL])
-// s->raw_prot = rta_getattr_u8(tb[INET_DIAG_PROTOCOL]);
+func (raw RouteAttrValue) ToProtocol() inetdiag.Protocol {
+	return inetdiag.Protocol(raw.toUint8())
+}
+
+func (raw RouteAttrValue) ToMark() uint32 {
+	if raw == nil || len(raw) < 4 {
+		return 0
+	}
+	return *(*uint32)(unsafe.Pointer(&raw[0]))
+}
 
 // ToBBRInfo maps the raw RouteAttrValue onto a BBRInfo.
 // For older data, it may have to copy the bytes.
 func (raw RouteAttrValue) ToBBRInfo() *inetdiag.BBRInfo {
 	structSize := (int)(unsafe.Sizeof(inetdiag.BBRInfo{}))
 	return (*inetdiag.BBRInfo)(maybeCopy(raw, structSize))
-}
-
-// ParseCong returns the congestion algorithm string
-// INET_DIAG_CONG
-func (raw RouteAttrValue) CongestionAlgorithm() string {
-	return string(raw)
 }
 
 // Parent containing all info gathered through netlink library.
@@ -423,23 +473,11 @@ type Wrapper struct {
 	// Metadata for the connection.  Usually empty.
 	Metadata *Metadata
 
+	// Bit field indicating whether each message type was observed.
+	FieldMask uint32
+
 	// Info from struct inet_diag_msg, including socket_id;
 	InetDiagMsg *inetdiag.InetDiagMsg
-
-	// From INET_DIAG_PROTOCOL message.
-	DiagProtocol inetdiag.Protocol
-
-	// From INET_DIAG_CONG message.
-	CongestionAlgorithm string
-
-	// The following three are mutually exclusive, as they provide
-	// data from different congestion control strategies.
-	//Vegas *VegasInfo
-	BBR *inetdiag.BBRInfo
-	//DCTCP *DCTCPInfo
-
-	// Data obtained from INET_DIAG_SKMEMINFO.
-	SocketMem *inetdiag.SocketMemInfo
 
 	// Data obtained from INET_DIAG_MEMINFO.
 	MemInfo *inetdiag.MemInfo
@@ -447,12 +485,25 @@ type Wrapper struct {
 	// Data obtained from struct tcp_info.
 	TcpInfo *tcp.LinuxTCPInfo
 
-	// TODO
-	// If there is shutdown info, this is the mask value.
-	// Check has_shutdown_mask to determine whether present.
-	//
-	// Types that are valid to be assigned to Shutdown:
-	//	*TCPDiagnosticsProto_ShutdownMask
-	// Shutdown isTCPDiagnosticsProto_Shutdown
+	VegasInfo *inetdiag.VegasInfo
 
+	// From INET_DIAG_CONG message.
+	CongestionAlgorithm string
+
+	TOS    uint8
+	TClass uint8
+
+	// Data obtained from INET_DIAG_SKMEMINFO.
+	SocketMem *inetdiag.SocketMemInfo
+
+	Shutdown uint8 // TODO do we need an indicator if this is present?
+
+	DCTCPInfo *inetdiag.DCTCPInfo
+
+	// From INET_DIAG_PROTOCOL message.
+	Protocol inetdiag.Protocol
+
+	BBRInfo *inetdiag.BBRInfo
+
+	Mark uint32
 }
