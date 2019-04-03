da2b0a59657be591482ac43700eef638f7cc92de
Greg Russell <gfr10598@gmail.com>
2019-04-03 17:36:14 -0400

    more tests for cmd/csvtool

--- cmd/csvtool/main_test.go
+++ cmd/csvtool/main_test.go
@@ -45,3 +45,41 @@ func TestConvertFileToCSV(t *testing.T) {
 		t.Errorf("%d\n%s\n%s\n:%s:\n", len(lines), lines[0], lines[1], lines[len(lines)-1])
 	}
 }
+
+func TestBCNFile(t *testing.T) {
+	stdout := os.Stdout
+
+	r, w, err := os.Pipe()
+	rtx.Must(err, "Failed to open pipe: ")
+	os.Stdout = w
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	var readErr error
+
+	src := "testdata/bcn01/ndt-jdczh_1553815964_00000000000003E8.00183.jsonl.zst"
+	go func() {
+		readErr = ConvertFileToCSV(src)
+		os.Stdout.Close()
+		wg.Done()
+	}()
+
+	// Must read the output to allow the conversion to proceed.
+	output, err := ioutil.ReadAll(r)
+	wg.Wait()
+	if readErr != nil {
+		// This is technically not legal, as testing is not fully threadsafe.
+		t.Fatal("Conversion problem", err)
+	}
+
+	os.Stdout = stdout
+
+	rtx.Must(err, "Problem reading output")
+
+	lines := strings.Split(string(output), "\n")
+	// Split introduces one final empty string, so with the header, the total is 153.
+	if len(lines) != 153 {
+		t.Errorf("%d\n%s\n%s\n:%s:\n", len(lines), lines[0], lines[1], lines[len(lines)-1])
+	}
+
+}
