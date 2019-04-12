443c6e4b63d3301f75f3ea6bae581660f8259333
Peter Boothe <pboothe@google.com>
2019-04-12 18:05:15 -0400

    100% coverage in two more packages (#79)
    * 100% coverage of structs
    * Refactored and 100% tested csvtool

--- cmd/csvtool/main_test.go
+++ cmd/csvtool/main_test.go
@@ -2,14 +2,69 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
+	"log"
+	"os"
 	"strings"
 	"testing"
+
+	"github.com/m-lab/go/rtx"
+	"github.com/m-lab/tcp-info/snapshot"
 )
 
+func TestMainTooManyArgs(t *testing.T) {
+	defer func(args []string) {
+		os.Args = args
+		logFatal = log.Fatal
+	}(os.Args)
+
+	os.Args = []string{"test_csvtool", "file1", "file2"}
+	logFatal = func(...interface{}) {
+		panic("panic instead of log.Fatal")
+	}
+
+	defer func() {
+		e := recover()
+		if e == nil {
+			t.Error("Should have panicked")
+		}
+	}()
+
+	main()
+}
+
+func TestMain(t *testing.T) {
+	defer func(args []string) {
+		os.Args = args
+	}(os.Args)
+
+	// Nothing crashes when we pass in a valid file.
+	os.Args = []string{"test_csvtool", "testdata/ndt-jdczh_1553815964_00000000000003E8.00183.jsonl.zst"}
+	main()
+}
+
+func TestOpenFile(t *testing.T) {
+	dir, err := ioutil.TempDir("", "TestOpenFile")
+	rtx.Must(err, "Could not make tempdir")
+	defer os.RemoveAll(dir)
+	rtx.Must(ioutil.WriteFile(dir+"/test.txt", []byte("abcd"), 0666), "Could not write test.txt")
+	r, err := openFile(dir + "/test.txt")
+	rtx.Must(err, "Could not open file")
+	b, err := ioutil.ReadAll(r)
+	rtx.Must(err, "Could not read file")
+	if string(b) != "abcd" {
+		t.Errorf("%q != \"abcd\"", string(b))
+	}
+}
+
 func TestFileToCSV(t *testing.T) {
-	src := "testdata/ndt-jdczh_1553815964_00000000000003E8.00183.jsonl.zst"
+	src, err := openFile("testdata/ndt-jdczh_1553815964_00000000000003E8.00183.jsonl.zst")
+	rtx.Must(err, "Could not open file")
 	buf := bytes.NewBuffer(nil)
-	err := fileToCSV(src, buf)
+	snaps, err := readSnapshots(src)
+	rtx.Must(err, "Could not read test data")
+
+	err = toCSV(snaps, buf)
 
 	if err != nil {
 		t.Fatal("Conversion problem", err)
@@ -44,5 +99,14 @@ func TestFileToCSV(t *testing.T) {
 	}
 	if record[15] != "3E8" {
 		t.Error(record[15])
+	}
+}
+
+func TestToCSVFillsInEmptyMetadata(t *testing.T) {
+	snap := snapshot.Snapshot{}
+	buf := bytes.NewBuffer(nil)
+	rtx.Must(toCSV([]*snapshot.Snapshot{&snap}, buf), "Could not convert")
+	if snap.Metadata == nil {
+		t.Error("Failed to fill in empty metadata")
 	}
 }
